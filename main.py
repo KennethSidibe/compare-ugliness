ff48fa106df021b490df0b4985d274bd023703e2
Sidibe <Elstevehcks@gmail.com>
2022-03-12 17:48:58 -0500

    test main file

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ IMAGE_TEST = "armandguigma.jpg"
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
     # Load image
     image = cv2.imread(IMAGE_TEST)
 
     landmarks = getFaceLandmarks(image)
 
-    drawFeaturesPoint(landmarks, image)
+    # drawFeaturesPoint(landmarks, image)
 
     x_reference = landmarks.part(27).x
     y_reference = landmarks.part(27).y
@@ -23,10 +22,6 @@ if __name__ == '__main__':
     x_right = landmarks.part(43).x
     y_right = landmarks.part(43).y
 
-    left_distance = getDistance(x_left, y_left, x_reference, y_reference)
-    right_distance = getDistance(x_reference, y_reference, x_right, y_right)
+    # symmetryRatioFaceFeature(landmarks)
 
-    print("left distance : ", left_distance)
-    print("right distance : ", right_distance)
-
-    showImage(image)
+    # showImage(image)
