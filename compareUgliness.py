71eea9f7f253bb1830cb2c30e850d96e278dc46a
Sidibe <Elstevehcks@gmail.com>
2022-03-14 18:18:35 -0400

    update repo

--- compareUgliness.py
+++ compareUgliness.py
@@ -1,6 +1,7 @@
 import math
 import cv2
 import dlib
+import os
 import matplotlib.pyplot as plt
 
 # landmarks that define the human face
@@ -16,8 +17,8 @@ LEFT_EYEBROW_POINTS = [17, 21]
 
 # Nose
 NOSE_POINTS = [27, 35]
-LEFT_NOSE_POINTS = [34, 35]
-RIGHT_NOSE_POINTS = [31, 32]
+LEFT_NOSE_POINTS = [31, 33]
+RIGHT_NOSE_POINTS = [34, 36]
 
 # Eyes
 LEFT_EYE_POINTS = [36, 41]
@@ -25,8 +26,8 @@ RIGHT_EYE_POINTS = [42, 47]
 
 # Mouths
 MOUTH_POINTS = [48, 60]
-LEFT_MOUTH_POINTS = [52, 53, 54, 55, 56]
-RIGHT_MOUTH_POINTS = [48, 49, 50, 58, 59]
+RIGHT_MOUTH_POINTS = [52, 53, 54, 55, 56]
+LEFT_MOUTH_POINTS = [48, 49, 50, 58, 59]
 
 # Lips
 LIPS_POINTS = [61, 67]
@@ -34,6 +35,7 @@ LEFT_LIPS_POINTS = [60, 61, 67]
 RIGHT_LIPS_POINTS = [63, 64, 65]
 
 SYMMETRY_LINE_POINTS = [27, 28, 29, 30, 33, 51, 62, 66, 57, 8]
+
 FACE_FEATURES = ["jaw",
                  "eyes",
                  "eye_brows",
@@ -42,8 +44,15 @@ FACE_FEATURES = ["jaw",
                  "lips"
                  ]
 
-RESIZED_HEIGHT = 200
-RESIZED_WIDTH = 200
+# Richard Reference ratio
+RICHARD_RATIO_JAW = 0.84
+RICHARD_RATIO_EYES = 1.239
+RICHARD_RATIO_EYE_BROWS = 2.654
+RICHARD_RATIO_NOSE = 1.288
+RICHARD_RATIO_MOUTH = 0.817
+RICHARD_RATIO_LIPS = 0.797
+RICHARD_RATIO = [RICHARD_RATIO_JAW, RICHARD_RATIO_EYES, RICHARD_RATIO_EYE_BROWS,
+                 RICHARD_RATIO_NOSE, RICHARD_RATIO_MOUTH, RICHARD_RATIO_LIPS]
 
 
 def printCoordinate(start, end, landmarks):
@@ -52,6 +61,24 @@ def printCoordinate(start, end, landmarks):
         y = landmarks.part(i).y
 
         print("point : ", i, "x : ", x, "y : ", y)
+
+
+def getAllFileInFolder(folderPath):
+    imagesPath = []
+
+    for root, directories, files in os.walk(folderPath):
+        for file in files:
+            filePath = os.path.join(root, file)
+            if file.endswith(".png"):
+                imagesPath.append(filePath)
+
+            if file.endswith(".jpg"):
+                imagesPath.append(filePath)
+
+            if file.endswith(".jpeg"):
+                imagesPath.append(filePath)
+
+    return imagesPath
 
 
 def getFaceLandmarks(image):
@@ -89,7 +116,55 @@ def getDistance(x1, y1, x2, y2):
 
 
 def estimateUgliness(landmarks):
-    print()
+    personRatioFeaturesList = getAllFaceFeaturesRatio(landmarks)
+    faceFeaturesRatio = {}
+    faceFeatureRatioList = []
+
+    for featureIndex in range(0, len(FACE_FEATURES)):
+        personRatio = personRatioFeaturesList[featureIndex]
+
+        faceFeature = FACE_FEATURES[featureIndex]
+
+        richardRatio = RICHARD_RATIO[featureIndex]
+
+        percentageFeature = compareRatio(richardRatio, personRatio)
+
+        faceFeaturesRatio[faceFeature] = percentageFeature
+
+        faceFeatureRatioList.append(percentageFeature)
+
+    beautyPercentage = averageValue(faceFeatureRatioList)
+    uglinessPercentage = 100 - beautyPercentage
+
+    return uglinessPercentage
+
+
+def getUglinessFromImage(imagePath):
+    name = getFileName(imagePath)
+
+    image = cv2.imread(imagePath)
+
+    personLandmarks = getFaceLandmarks(image)
+
+    personUglinessPercentage = estimateUgliness(personLandmarks)
+
+    personUglinessOn10 = percentageTo10(personUglinessPercentage)
+
+    return personUglinessOn10
+
+
+def getFileName(filePath):
+    filename = os.path.basename(filePath)
+    separator = '.'
+    name = filename.split(separator, 1)[0]
+    return name
+
+
+def compareRatio(richardRatio, personRatio):
+    if richardRatio > personRatio:
+        return round(((100 * personRatio) / richardRatio), 2)
+    else:
+        return round(((100 * richardRatio) / personRatio), 2)
 
 
 def drawFeaturesPoint(landmarks, image):
@@ -103,9 +178,60 @@ def drawFeaturesPoint(landmarks, image):
         cv2.circle(img=image, center=(x, y), radius=4, color=(0, 255, 0), thickness=-1)
 
 
-def symmetryRatioFaceFeature(faceFeature):
-    #     TODO
-    print()
+def getAllFaceFeaturesRatio(landmarks):
+    featureRatio = []
+
+    for feature in FACE_FEATURES:
+        featureRatio.append(symmetryRatioFaceFeature(landmarks, feature))
+
+    return featureRatio
+
+
+def symmetryRatioFaceFeature(landmarks, faceFeature):
+    faceFeaturesCoordinates = getAllCoordinateFaceFeature(landmarks)
+    centerFaceX = getCenterFaceX(landmarks)
+    ratioFeatureList = []
+
+    if faceFeature == FACE_FEATURES[0]:
+        faceCoordinateIndex = 0
+
+    elif faceFeature == FACE_FEATURES[1]:
+        faceCoordinateIndex = 1
+
+    elif faceFeature == FACE_FEATURES[2]:
+        faceCoordinateIndex = 2
+
+    elif faceFeature == FACE_FEATURES[3]:
+        faceCoordinateIndex = 3
+
+    elif faceFeature == FACE_FEATURES[4]:
+        faceCoordinateIndex = 4
+
+    elif faceFeature == FACE_FEATURES[5]:
+        faceCoordinateIndex = 5
+
+    leftFeatureCoordinate_X = faceFeaturesCoordinates[faceCoordinateIndex][0]
+    leftFeatureCoordinate_Y = faceFeaturesCoordinates[faceCoordinateIndex][1]
+
+    rightFeatureCoordinate_X = faceFeaturesCoordinates[faceCoordinateIndex][2]
+    rightFeatureCoordinate_Y = faceFeaturesCoordinates[faceCoordinateIndex][3]
+
+    for index in range(0, len(leftFeatureCoordinate_X)):
+        leftPoint_X = leftFeatureCoordinate_X[index]
+        leftPoint_Y = leftFeatureCoordinate_Y[index]
+
+        rightPoint_X = rightFeatureCoordinate_X[index]
+        rightPoint_Y = rightFeatureCoordinate_Y[index]
+
+        centerPoint_Y = centerOfPoints([leftPoint_Y, rightPoint_Y])
+
+        leftDistance = getDistance(leftPoint_X, leftPoint_Y, centerFaceX, centerPoint_Y)
+        rightDistance = getDistance(rightPoint_X, rightPoint_Y, centerFaceX, centerPoint_Y)
+        ratio = leftDistance / rightDistance
+        ratio = round(ratio, 3)
+        ratioFeatureList.append(ratio)
+
+    return averageValue(ratioFeatureList)
 
 
 def centerOfPoints(coordinates):
@@ -253,7 +379,7 @@ def getCoordinateFaceFeature(feature, landmarks):
             leftCoordinateFeature_X.append(pointCoordinate_X)
             leftCoordinateFeature_Y.append(pointCoordinate_Y)
 
-        for point_index in LEFT_MOUTH_POINTS:
+        for point_index in RIGHT_MOUTH_POINTS:
             pointCoordinate_X = landmarks.part(point_index).x
             pointCoordinate_Y = landmarks.part(point_index).y
 
@@ -293,6 +419,25 @@ def getCoordinateFaceFeature(feature, landmarks):
         return coordinates
 
 
+def averageValue(valueList):
+    sumRatio = 0
+    for ratio in valueList:
+        sumRatio += ratio
+
+    average = sumRatio / len(valueList)
+    return round(average, 3)
+
+
+def getCenterFaceX(landmarks):
+    sumPoint = 0
+
+    for pointIndex in SYMMETRY_LINE_POINTS:
+        x = landmarks.part(pointIndex).x
+        sumPoint += x
+
+    return sumPoint / len(SYMMETRY_LINE_POINTS)
+
+
 def getAllCoordinateFaceFeature(landmarks):
     leftCoordinateFeature_X = []
     leftCoordinateFeature_Y = []
@@ -314,7 +459,7 @@ def getAllCoordinateFaceFeature(landmarks):
     COORDINATES_EYES = getCoordinateFaceFeature(FACE_FEATURES[1], landmarks)
 
     # Coordinates of eye_brows points
-    COORDINATES_EYES = getCoordinateFaceFeature(FACE_FEATURES[2], landmarks)
+    COORDINATES_EYE_BROWS = getCoordinateFaceFeature(FACE_FEATURES[2], landmarks)
 
     # Coordinates of Nose points
     COORDINATES_NOSE = getCoordinateFaceFeature(FACE_FEATURES[3], landmarks)
@@ -323,7 +468,12 @@ def getAllCoordinateFaceFeature(landmarks):
     COORDINATES_MOUTH = getCoordinateFaceFeature(FACE_FEATURES[4], landmarks)
 
     # Coordinates of lips points
-    COORDINATES_EYES = getCoordinateFaceFeature(FACE_FEATURES[5], landmarks)
+    COORDINATES_LIPS = getCoordinateFaceFeature(FACE_FEATURES[5], landmarks)
+
+    faceFeaturesCoordinates = [COORDINATES_JAW, COORDINATES_EYES, COORDINATES_EYE_BROWS, COORDINATES_NOSE,
+                               COORDINATES_MOUTH, COORDINATES_LIPS]
+
+    return faceFeaturesCoordinates
 
 
 def emptyCoordinateList(list_x, list_y):
@@ -331,23 +481,14 @@ def emptyCoordinateList(list_x, list_y):
     list_y[:] = []
 
 
-def symmetryRatioFaceFeature(landmarks):
-    coordinateSymmetryLine_X = []
-    coordinateSymmetryLine_Y = []
+def percentageToScale(percentage, scaleLimit):
+    scale = (percentage * scaleLimit) / 100
 
-    for i in SYMMETRY_LINE_POINTS:
-        x = landmarks.part(i).x
-        y = landmarks.part(i).y
-        coordinateSymmetryLine_X.append(x)
-        coordinateSymmetryLine_Y.append(y)
+    return round(scale)
 
-    SYMMETRY_X_COORDINATE = centerOfPoints(coordinateSymmetryLine_X)
 
-    for feature in FACE_FEATURES:
-        leftEyeCoordinate_X = []
-        leftEyeCoordinate_y = []
-
-        # todo
+def percentageTo10(percentage):
+    return percentageToScale(percentage, 10)
 
 
 def showImage(image):
@@ -359,16 +500,28 @@ def showImage(image):
     # Close all windows
     cv2.destroyAllWindows()
 
+
+def showImageTime(image, time):
+    cv2.imshow("Picture", mat=image)
+
+    # Wait for a key press to exit
+    cv2.waitKey(delay=time*1000)
+
+    # Close all windows
+    cv2.destroyAllWindows()
+
 # # Resize Image
 # resized = cv2.resize(image, (RESIZED_WIDTH, RESIZED_HEIGHT))
 
 # # Rotate image
 # image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
-#
+
+
 # # Other ways of rotating
 # rows, cols = image.shape[:2]
 # deg = 45
-#
+
+
 # # (col/2,rows/2) is the center of rotation for the image
 # # M is the coordinates of the center
 # M = cv2.getRotationMatrix2D((cols/2, rows/2), deg, 1)
--- main.py
+++ main.py
@@ -1,27 +1,130 @@
 import dlib
 import cv2
+import time
 from compareUgliness import *
 
 IMAGE_TEST = "armandguigma.jpg"
+RICHARD_PIC = "Richardphoto.png"
+
+FRIENDS_IMAGE_FOLDER_PATH = rf"C:\Users\Kenneth Sidibe\PycharmProjects\compareUgliness\MGTC Pic"
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # Load image
-    image = cv2.imread(IMAGE_TEST)
 
-    landmarks = getFaceLandmarks(image)
+    imagesPath = getAllFileInFolder(FRIENDS_IMAGE_FOLDER_PATH)
+
+    # Load image
+    # image = cv2.imread(IMAGE_TEST)
+
+    # landmarks = getFaceLandmarks(image)
 
     # drawFeaturesPoint(landmarks, image)
 
-    x_reference = landmarks.part(27).x
-    y_reference = landmarks.part(27).y
+    # percentage = estimateUgliness(landmarks)
+    uglinessRate = []
 
-    x_left = landmarks.part(38).x
-    y_left = landmarks.part(38).y
+    print("BIENVENUE sur le calculateur de vilainété !")
+    print()
+    time.sleep(5)
+    print("J'ai deja les images de tout le monde donc on peut commencer a se comparer")
 
-    x_right = landmarks.part(43).x
-    y_right = landmarks.part(43).y
+    time.sleep(5)
+    print("Voici l'homme le plus beau le plus frais le plus choco sur lequel je me baserai pour vous evaluer : ")
+    richardImage = cv2.imread(RICHARD_PIC)
+    showImageTime(richardImage, 5)
+    print()
 
-    # symmetryRatioFaceFeature(landmarks)
+    print("Bon j'espere que vous avez pu contempler le beau passons maintenant au test")
+    print()
+    time.sleep(5)
 
-    # showImage(image)
+    print("Voici la premiere personnes que nous allons estimer la beaute ")
+    name = getFileName(imagesPath[0])
+    image = cv2.imread(imagesPath[0])
+    showImageTime(image, 5)
+    print("Voici : ", name, "cherchons sur une echelle de 1 a 10 combien il est vilain")
+    print()
+    print("Loading vilainete...")
+    personUgliness = getUglinessFromImage(imagesPath[0])
+    uglinessRate.append(personUgliness)
+    print("Alors ", name, " est ", personUgliness, "fois vilain sur une echelle de 1 a 10")
+    print()
+    time.sleep(5)
+
+    print("Voici la deuxieme personne que nous allons estimer la beaute ")
+    image = cv2.imread(imagesPath[1])
+    name = getFileName(imagesPath[1])
+    showImageTime(image, 5)
+    print("Voici : ", name, "cherchons sur une echelle de 1 a 10 combien il est vilain")
+    print()
+    print("Loading vilainete...")
+    personUgliness = getUglinessFromImage(imagesPath[1])
+    uglinessRate.append(personUgliness)
+    print("Alors ", name, " est ", personUgliness, "fois vilain sur une echelle de 1 a 10")
+    print()
+    time.sleep(5)
+
+    print("Voici la troisieme personne que nous allons estimer la beaute ")
+    image = cv2.imread(imagesPath[2])
+    name = getFileName(imagesPath[2])
+    showImageTime(image, 5)
+    print("Voici : ", name, "cherchons sur une echelle de 1 a 10 combien il est vilain")
+    print()
+    print("Loading vilainete...")
+    personUgliness = getUglinessFromImage(imagesPath[2])
+    uglinessRate.append(personUgliness)
+    print("Alors ", name, " est ", personUgliness, "fois vilain sur une echelle de 1 a 10")
+    print()
+    time.sleep(5)
+
+    print("Voici la quatrieme personne que nous allons estimer la beaute ")
+    image = cv2.imread(imagesPath[3])
+    name = getFileName(imagesPath[3])
+    showImageTime(image, 5)
+    print("Voici : ", name, "cherchons sur une echelle de 1 a 10 combien il est vilain")
+    print()
+    print("Loading vilainete...")
+    personUgliness = getUglinessFromImage(imagesPath[3])
+    uglinessRate.append(personUgliness)
+    print("Alors ", name, " est ", personUgliness, "fois vilain sur une echelle de 1 a 10")
+    print()
+    time.sleep(5)
+
+    print("Voici la cinquieme personne que nous allons estimer la beaute ")
+    image = cv2.imread(imagesPath[4])
+    name = getFileName(imagesPath[4])
+    showImageTime(image, 5)
+    print("Voici : ", name, "cherchons sur une echelle de 1 a 10 combien il est vilain")
+    print()
+    print("Loading vilainete...")
+    personUgliness = getUglinessFromImage(imagesPath[4])
+    uglinessRate.append(personUgliness)
+    print("Alors ", name, " est ", personUgliness, "fois vilain sur une echelle de 1 a 10")
+    print()
+    time.sleep(5)
+
+    print("Voici la sixieme personne que nous allons estimer la beaute ")
+    image = cv2.imread(imagesPath[5])
+    name = getFileName(imagesPath[5])
+    showImageTime(image, 5)
+    print("Voici : ", name, "cherchons sur une echelle de 1 a 10 combien il est vilain")
+    print()
+    print("Loading vilainete...")
+    personUgliness = getUglinessFromImage(imagesPath[5])
+    uglinessRate.append(personUgliness)
+    print("Alors ", name, " est ", personUgliness, "fois vilain sur une echelle de 1 a 10")
+    print()
+    time.sleep(5)
+
+    uglinessRate.sort()
+
+    print("voici le plus vilain maintenant :")
+    time.sleep(1)
+    print(".")
+    time.sleep(1)
+    print("..")
+    time.sleep(1)
+    print("...")
+
+    showImageTime(cv2.imread(imagesPath[5]), 5)
+
